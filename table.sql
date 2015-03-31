27f8623f8b6f1855cbf94bb5c61cee50b1449a30
Igor Zvyagintsev <inoyakaigor@ya.ru>
2015-04-01 02:12:26 +0300

    Update
    replace 'prcnt' column to 'order';
    auto increment set to 0;
    fixed comment;
    change character set to utf8

--- table.sql
+++ table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `tournaments` (
   `id` smallint(5) NOT NULL AUTO_INCREMENT,
   `tournament` text NOT NULL,
-  `team` text CHARACTER SET cp1251 NOT NULL,
+  `team` text CHARACTER SET utf8 NOT NULL,
   `teamlink` text,
   `i` varchar(20) DEFAULT NULL,
   `v` varchar(20) DEFAULT NULL,
@@ -9,6 +9,6 @@ CREATE TABLE `tournaments` (
   `p` varchar(20) DEFAULT NULL,
   `m` varchar(20) DEFAULT NULL,
   `o` int(5) DEFAULT '0',
-  `prcnt` varchar(20) NOT NULL,
+  `order` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Турнирные таблицы' AUTO_INCREMENT=148 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='РўСѓСЂРЅРёСЂРЅС‹Рµ С‚Р°Р±Р»РёС†С‹' AUTO_INCREMENT=0 ;
